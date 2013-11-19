aff8b788006608b25a89ac8b04b5622249207886
Dash <dashwedergren@gmail.com>
2013-11-19 07:22:18 -0600

    Added console.log

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 var hash = require('./module');
 var times = 4;
+//number of times the program calculates 200,000 hashes
 var time = hash.Pbkdf2(times);
+//type of hash in this case it is Pbkdf2
+//you can use Md5 Aes Bcrypt Sha-1
+console.log(time);
+//logs time to the command prompt
